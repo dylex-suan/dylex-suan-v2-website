f5a59a86a644fc863cf3e2022126a17880c0154d
Dylex Suan <dylexms@gmail.com>
2022-01-24 22:51:16 -0500

    Get the data in

---
+++ src/data/project-display.jsx
@@ -0,0 +1,27 @@
+import covid_watchdog from '../assets/covid_watchdog.png';
+import insuratory from '../assets/insuratory.png';
+import not_newspaper from '../assets/not_newspaper.png';
+
+export const project_display = [
+    {
+        image: covid_watchdog,
+        id: 1,
+        title: "COVID Watchdog",
+        technologies: ["REACT.JS", "PYTHON", "CSS"],
+        description: "Web application to help people decide whether it's safe to visit a province based on number of COVID cases."
+    },
+    {
+        image: insuratory,
+        id: 2,
+        title: "Insuratory",
+        technologies: ["REACT NATIVE", "PYTHON", "CSS"],
+        description: "Mobile application to help people collate their belongings and organize them for insurance purposes."
+    },
+    {
+        image: not_newspaper,
+        id: 3,
+        title: "This Is Not A Newspaper",
+        technologies: ["REACT.JS", "NODE.JS", "EXPRESS.JS", "CSS"],
+        description: "Web application to collect various news and present it in a simple and organized way."
+    },
+];
