bf847a1af5eac035668c81c812c423161f7e1e93
Dylex Suan <dylexms@gmail.com>
2022-01-24 22:51:24 -0500

    Start showing the components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import './fonts/fonts.css';
+import styled from 'styled-components';
+import profile_photo from './assets/profile_photo.png';
+import { About } from './components/About';
+import { Projects } from './components/Projects';
 
 export const App = () => {
   return (
-    <div></div>
+    <Container>
+      <Image src={profile_photo} alt='' />
+      <About />
+      <Projects />
+    </Container>
   )
 }
+
+
+const Container = styled.div`
+  background: #FFF9F9;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  font-family: "Open-Sans", sans-serif;
+`;
+
+const Image = styled.img`
+  width: 449px;
+  height: 449px;
+  padding: 15px 0px;
+  align-items: center;
+  justify-content: center;
+`;
