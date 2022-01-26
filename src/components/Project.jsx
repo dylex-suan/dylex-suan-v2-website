d02413050a240c3a81cd474a8865a77612ce2858
Dylex Suan <dylexms@gmail.com>
2022-01-25 23:26:09 -0500

    Start styling the projects area

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,17 +4,31 @@ export const Project = (props) => {
     const { data } = props;
     return (
         <Container>
-            <Image src={data.image} alt='' />
-            <Heading>
-                {data.title}
-            </Heading>
-            <Remote>Hello</Remote>
+            <ImageWrapper>
+                <Image src={data.image} alt='' />
+            </ImageWrapper>
+            <DescriptionWrapper>
+                <Heading>
+                    {data.title}
+                </Heading>
+                <RemoteWrapper>
+                    {data.technologies.map((tech, id) => {
+                        return <Remote key={id}>
+                            <RemoteText>
+                                {tech}
+                            </RemoteText>
+                        </Remote>
+                    })}
+                </RemoteWrapper>
+                <Description>{data.description}</Description>
+            </DescriptionWrapper>
         </Container>
     )
 }
 
 const Container = styled.div`
     display: flex;
+    align-items: center;
     width: 900px;
     height: 300px;
     background-color: #F1EBEB;
@@ -22,7 +36,17 @@ const Container = styled.div`
     border-radius: 50px;
 `;
 
+const ImageWrapper = styled.div`
+
+`;
 const Image = styled.img`
+    width: 360px;
+    height: 240px;
+    radius: 46px;
+    margin: auto 25px;
+`;
+
+const TextWrapper = styled.img`
     width: 360px;
     height: 240px;
     radius: 46px;
@@ -31,17 +55,33 @@ const Image = styled.img`
 
 const Heading = styled.h1`
     font-size: 36px;
-    margin: 25px 25px;
+    align-items: center;
+    ${'' /* margin: 25px 25px; */}
 `;
 
 const Remote = styled.div`
-    width: 120px;
+    display: flex;
+    width: 100%;
     height: 40px;
+    margin: 0 10px 0 0;
     background-color: #C4C4C4;
-    text-align: center;
     border-radius: 50px;
+    align-items: center;
+    justify-content: center;
 `;
 
 const DescriptionWrapper = styled.div`
+    width: 450px;
+`;
 
+const Description = styled.p`
+    font-size: 20px;
+`;
+
+const RemoteText = styled.p`
+    font-size: 18px;
+`
+const RemoteWrapper = styled.div`
+    display: flex;
+    justify-content: flex-start;
 `;
