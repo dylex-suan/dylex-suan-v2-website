97685b2d05a097146d9447190fdf46879eef1b09
Dylex Suan <dylexms@gmail.com>
2022-02-03 22:23:27 -0500

    About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,18 +3,11 @@ import styled from "styled-components";
 export const About = () => {
     return (
         <Wrapper>
-            <Greeting>Hi! My name is Dylex Suan!</Greeting>
+            <Greeting>Hi! I'm Dylex Suan!👋🏽</Greeting>
             <Body>
-                I am currently a 2B Computer Science Student at the University of Waterloo and am currently completing my second internship at Prodigy Game Education!<br /><br />
-
+                I am currently a 2B Computer Science Student at the University of Waterloo and am currently completing my internship at Prodigy!<br /><br />
                 I was previously a full-stack developer at the University of Waterloo, working on the Math Faculty Question Bank for professors and teaching assistants!<br /><br />
-
                 Fun fact, I was previously a French journalist in the Greater Toronto area, and a Student Trustee for the Halton Catholic District School Board!<br /><br />
-
-                {/* In my free time, I like to
-                play piano 🎹
-                read 📚
-                learn languages! */}
             </Body>
         </Wrapper>
     )
@@ -25,7 +18,7 @@ const Greeting = styled.h1`
 `;
 
 const Wrapper = styled.div`
-    padding: 0px 200px;
+    max-width: 90%;
     line-height: 1.5;
 `;
 
